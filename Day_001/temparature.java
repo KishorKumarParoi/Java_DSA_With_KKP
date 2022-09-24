c9cfb4512df8922c56e884cf91d2cc7a53ecdcbf
KishorKumarParoi <kishorkumarparoi.ruet@gmail.com>
2022-09-24 14:49:42 +0600

    First day Done

---
+++ Day_001/temparature.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class temparature {
+    public static void main(String[] args) {
+        double f;
+        System.out.print("Enter Farenhite Temparature : ");
+        Scanner input = new Scanner(System.in);
+        f = input.nextDouble();
+        double c = (5*f-160)/9;
+        System.out.println(c);
+    }
+}
---
+++ Day_001/typeCasting.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class typeCasting {
+    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        int a = input.nextInt();
+//        int b = input.nextInt();
+//        float c = a + b;
+//        c += 34.343F;
+//        System.out.println(c);
+//
+//        //type casting
+//        int k = (int)c;
+//        System.out.println(k);
+        int aa = 257;
+        byte b = (byte)aa;
+        b = 50;
+        System.out.println(b*2);
+    }
+}
