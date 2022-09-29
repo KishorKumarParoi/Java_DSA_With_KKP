19b3ed8a828c6c5b14ff1ad7300ea3dbaa704822
KishorKumarParoi <kishorkumarparoi.ruet@gmail.com>
2022-09-29 12:17:28 +0600

    renamed folder

---
+++ Conditional-loops/Switch.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double grade = in.nextDouble();
+
+        switch (grade){
+            f
+        }
+    }
+}
--- Day_002/Switch.java
+++
@@ -1,5 +0,0 @@
-public class Switch {
-    public static void main(String[] args) {
-        
-    }
-}
