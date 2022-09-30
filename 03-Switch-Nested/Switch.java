302663cd8c01d71c8fd6bf5baf8b2b68c3f09544
KishorKumarParoi <kishorkumarparoi.ruet@gmail.com>
2022-09-30 08:34:54 +0600

    deleted day_002

--- 03-Switch-Nested/Switch.java
+++ 03-Switch-Nested/Switch.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class Switch {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println(in.next());
+        double grade = in.nextDouble();
+
+        switch (grade){
+            f
+        }
     }
 }
